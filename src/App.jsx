5bdc6eae2b2021a17ab24c5dfa9afc313a6511c0
Anni <anni.a.virtanen@tuni.fi>
2025-10-09 13:03:24 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// src/App.jsx
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <section className="landing">
+        <h1 className="title">aaltos</h1>
+      </section>
+      <section className="about">
+        <div className="about-content">
+          <h2>Olemme aaltos</h2>
+          <p>Entisistä ja nykyisistä tekniikanopiskelijoista koottu bilebändi aaltos 
+            soittaa kiinnostavaa musiikkia persoonallisella otteella. aaltos on niin 
+            cool että sen nimi kirjoitetaan pienellä alkukirjaimella. Bändiläiset ovat 
+            tuttuja konkareita useista opiskelijabändeistä, ja toistaiseksi bändi soittaa 
+            vain cover-musiikkia, mutta saa nähdä mitä tulevaisuus tuo tullessaan! 
+            Bändi on tilattavissa rahalla aivan mihin vain tilaisuuteen, ja yhteyden 
+            heihin saa instagramin tai sähköpostin kautta.</p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
