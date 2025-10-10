7bf01f1aa5901ec7e21abbb7842a8c457fbd0727
Anni <anni.a.virtanen@tuni.fi>
2025-10-10 18:06:45 +0300

    outline of the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,32 @@ function App() {
   return (
     <div>
       <section className="landing">
-        <h1 className="title">aaltos</h1>
+        {/* <img src="/src/assets/bg-1-fullsize-cropped.jpg" alt="background image" className="landingImage"/> */}
+        <div className="mainLogo">
+          <img src="/src/assets/aaltos-logo.svg" alt="aaltos band" className="logo"/>
+        </div>
+        <div className="menuMain">
+            <span>Tilaa meidät</span>
+            <span>Bändijäsenet</span>
+        </div>
+        <div className="menuIcons">
+          <img src="/src/assets/instagram-logo-33x33.svg" alt="Instagram icon" className="icon"/>
+          <img src="/src/assets/email-logo-38x33.svg" alt="Email icon" className="icon"/>
+        </div>
+
       </section>
       <section className="about">
         <div className="about-content">
           <h2>Olemme aaltos</h2>
-          <p>Entisistä ja nykyisistä tekniikanopiskelijoista koottu bilebändi aaltos 
+          <p>
+            Entisistä ja nykyisistä tekniikanopiskelijoista koottu bilebändi aaltos 
             soittaa kiinnostavaa musiikkia persoonallisella otteella. aaltos on niin 
             cool että sen nimi kirjoitetaan pienellä alkukirjaimella. Bändiläiset ovat 
             tuttuja konkareita useista opiskelijabändeistä, ja toistaiseksi bändi soittaa 
             vain cover-musiikkia, mutta saa nähdä mitä tulevaisuus tuo tullessaan! 
             Bändi on tilattavissa rahalla aivan mihin vain tilaisuuteen, ja yhteyden 
-            heihin saa instagramin tai sähköpostin kautta.</p>
+            heihin saa instagramin tai sähköpostin kautta.
+            </p>
         </div>
       </section>
     </div>
